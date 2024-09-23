7db32cb340d4f20bcf75b5a04a6f5a0dffc6bc8e
guoliangzhu <zhuguoliang@yunzhangfang.com>
2024-09-23 23:27:29 +0800

    freemark

---
+++ freemarker-demo/src/main/resources/templates/hi.ftl
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Hello World!</title>
+</head>
+<body>
+<b>普通文本 String 展示：</b><br><br>
+Hello ${name} <br>
+</body>
+</html>
---
+++ freemarker-demo/src/test/java/com/example/freemarker/freemarker_demo/FreemarkerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freemarker.freemarker_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreemarkerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
